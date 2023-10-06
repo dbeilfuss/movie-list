cc75371bddaa851389b6a44df753b8155a4f1bf7
Daniel Beilfuss <d.beilfuss@icloud.com>
2023-10-05 19:10:58 -0600

    finished basic lab

--- index.js
+++ index.js
@@ -1,34 +1,42 @@
-// const inputField = document.querySelector("input");
-
-// const addMovie = (event) => {
-//   event.preventDefault();
-//   const movieName = inputField.value;
-//   const listItem = document.createElement("li");
-
-//   listItem.textContent = movieName;
-
-//   const movieList = document.querySelector("ul");
-//   movieList.appendChild(listItem);
-
-//   inputField.value = "";
-// };
-
-// const form = document.querySelector("form");
-// form.addEventListener("submit", addMovie);
+const message = document.querySelector("#message");
 
 const inputField = document.querySelector("input");
 
 function addMovie(event) {
   event.preventDefault();
 
+  // add the movie to the list
   const movieName = inputField.value;
   const newMovie = document.createElement("li");
   const movieList = document.querySelector("ul");
 
   newMovie.textContent = movieName;
+  newMovie.addEventListener("click", crossOffMovie);
   movieList.appendChild(newMovie);
 
+  // clear the input field
   inputField.value = "";
+
+  // add a delete button
+  const deleteBtn = document.createElement("button");
+  deleteBtn.textContent = "X";
+  deleteBtn.addEventListener("click", deleteMovie);
+  newMovie.appendChild(deleteBtn);
+}
+
+function deleteMovie(event) {
+  message.textContent = "Movie Deleted";
+  event.target.parentNode.remove();
+}
+
+function crossOffMovie(event) {
+  let thisMovie = event.target;
+  thisMovie.classList.toggle("checked");
+  if (thisMovie.classList.contains("checked")) {
+    message.textContent = "Movie Watched!";
+  } else {
+    message.textContent = "Movie added back";
+  }
 }
 
 const form = document.querySelector("form");
